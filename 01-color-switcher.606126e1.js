cd8d7bc33bbf2605b22400e11f93ffcb730e7167
Minaht007 <108063187+Minaht007@users.noreply.github.com>
2023-01-16 12:25:21 +0000

    Deploying to gh-pages from @ Minaht007/goit-js-hw-09@9dd447c2be9da542d90988f7cab429a488b38981 🚀

---
+++ 01-color-switcher.606126e1.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector("button[data-start]");console.log(o);var t=document.querySelector("button[data-stop]");console.log(t);var e=document.querySelector("body");console.log(e)}();
+//# sourceMappingURL=01-color-switcher.606126e1.js.map
