8cc5b4b030d19ca54c5253e64812f954c5cb69c1
Andrey <49126678+Minaht@users.noreply.github.com>
2023-01-16 15:18:09 +0200

    change collor

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,13 +1,23 @@
 
 const startBtn = document.querySelector('button[data-start]')
-console.log(startBtn)
+// console.log(startBtn)
 const stopBtn = document.querySelector('button[data-stop]')
-console.log(stopBtn)
+// console.log(stopBtn)
 const body = document.querySelector('body');
-console.log(body)
-
-
+// console.log(body)
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+ 
+let timerForColor = [];
+
+startBtn.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+   timerForColor = setInterval(() => {
+       body.style.background = getRandomHexColor();
+   }, 1000);
+}
+// console.log(onChangeColor);
+
