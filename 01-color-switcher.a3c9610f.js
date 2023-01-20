59fcd2d8dcd8dc25b68ce8692a27de081a6cade9
Minaht007 <108063187+Minaht007@users.noreply.github.com>
2023-01-20 11:23:29 +0000

    Deploying to gh-pages from @ Minaht007/goit-js-hw-09@8648d7aea6f083ae2ad8d69da82bc7058caefe93 🚀

--- 01-color-switcher.a3c9610f.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let n=[];t.addEventListener("click",(function(){n=setInterval((()=>{d.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.disabled=!0,e.disabled=!1}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.a3c9610f.js.map
